90c973cc7da2ee5710833261afa5d4a66d370112
Amy Tims <amy.tims94@gmail.com>
2026-02-10 14:30:03 +1100

    editing file paths

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # Who am I assembling?
-GENUS_SPECIES="PseudomugilHalophilus3240756"
+OUTPUT_DIRECTORY="PseudomugilHalophilus3240756" # directory name on Pawsey where everything will run from
 
 # where's its script on github?
-BRANCH="p_halophilus"
+BRANCH="p_halophilus" # github branch name
 
 # make directory to put assembly scripts in
-mkdir /home/atims/afgi_assemblies/${GENUS_SPECIES} -p
+mkdir /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY} -p
 
 # create scratch directories
-mkdir /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/results -p
-mkdir /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/work
-mkdir /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/nextflow_logs
-mkdir /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/.snakemake
+mkdir /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/results -p
+mkdir /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/work
+mkdir /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/nextflow_logs
+mkdir /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/.snakemake
 
 # link to home dir
-ln -s /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/results /home/atims/afgi_assemblies/${GENUS_SPECIES}/results
-ln -s /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/work /home/atims/afgi_assemblies/${GENUS_SPECIES}/work
-ln -s /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/nextflow_logs /home/atims/afgi_assemblies/${GENUS_SPECIES}/nextflow_logs
-ln -s /scratch/pawsey1132/atims/afgi_assemblies/${GENUS_SPECIES}/.snakemake /home/atims/afgi_assemblies/${GENUS_SPECIES}/.snakemake
+ln -s /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/results /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/results
+ln -s /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/work /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/work
+ln -s /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/nextflow_logs /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/nextflow_logs
+ln -s /scratch/pawsey1132/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/.snakemake /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/.snakemake
 
 # pull in the script to run 
-wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/launcher.sh -O /home/atims/afgi_assemblies/${GENUS_SPECIES}/launcher.sh
+wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/launcher.sh -O /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/launcher.sh
 
 # pull in the short read qc script to run (comment out if not needed)
-wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/short-read-qc.sh -O /home/atims/afgi_assemblies/${GENUS_SPECIES}/short-read-qc.sh
+wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/short-read-qc.sh -O /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/short-read-qc.sh
 
 # pull in ONT qc script to run (comment out if not needed)
-wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/ont-qc.sh -O /home/atims/afgi_assemblies/${GENUS_SPECIES}/ont-qc.sh
+wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/ont-qc.sh -O /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/ont-qc.sh
 
 # pull in the sangertol config file
-wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/sangertol-nf.config -O /home/atims/afgi_assemblies/${GENUS_SPECIES}/sangertol-nf.config
+wget https://raw.githubusercontent.com/amytims/atol-genomelauncher-utils/refs/heads/${BRANCH}/sangertol-nf.config -O /home/atims/afgi_assemblies/${OUTPUT_DIRECTORY}/sangertol-nf.config
